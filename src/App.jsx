da3770100fe37bbfeb549515d7004c83928d51cc
unknown <rogel.cristobal11@gmailcom>
2023-02-09 08:35:32 +0800

    first commit on vite project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p className='text-3xl text-blue-500'>hello world</p>
+  )
+}
+
+export default App
