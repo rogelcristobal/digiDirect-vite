1b3247c25308686acdd0fe32e20b6d376b9d69f6
rogel cristobal <rogel.cristobal11@gmail.com>
2023-02-09 13:53:15 +0800

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ function App() {
       {/* navbar */}
       <div className="h-16 medium-box-divider box-border sticky top-0 "></div>
       {/* body */}
-      <div className="thin-box-divider container px-12 py-6 min-h-screen h-[200rem] flex  items-start justify-center mx-auto box-border">
+      <div className="thin-box-divider container px-12 py-6 min-h-screen h-[200rem] flex  gap-2 items-start justify-center mx-auto box-border">
         {/* sidebar */}
         <div className="h-[calc(100vh-5.5rem)] sticky top-24  w-80 medium-box-divider "></div>
         {/* content */}
         <div className="h-full medium-box-divider w-full"></div>
       </div>
+      {/* footer */}
+      <div className="h-80 w-full thin-box-divider"></div>
     </div>
   );
 }
