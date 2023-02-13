fbaff240a2cc8f93efa8ffb58aedaf44e27200d4
rogel cristobal <rogel.cristobal11@gmail.com>
2023-02-13 12:15:25 +0800

    created a function to convert text to camelcase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ const Navbar = () => {
      <div className="h-16 flex items-center justify-end thin-bottom-divider bg-inherit z-50 box-border sticky top-0 ">
         {/* items container */}
         <div className='h-fit container mx-auto items-center justify-end box-border flex thin-bottom-divider`'>
-            <span className='text-sm font-semibold capitalize mr-52 '>docs</span>
+           <nav className='thin-box-divider'>
+             <span className='text-sm font-semibold capitalize mr-52 '>docs</span>
+
+           </nav>
         </div>
      </div>
   );
--- src/components/SidebarLayoutPage.jsx
+++ src/components/SidebarLayoutPage.jsx
@@ -1,38 +1,96 @@
 import React from "react";
-import { useContext, useRef,useState,useEffect } from "react";
+import { useContext, useRef, useState, useEffect } from "react";
 import DocsContext from "../context/DocsContext";
 import Section from "./Section";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 const SidebarLayoutPage = () => {
   const { state: textContent } = useContext(DocsContext);
-  const ref=useRef(null)
+  const ref = useRef(null);
   const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
-    const handleScroll=()=>{
-      const position = Math.floor(window.scrollY)
-      
-      if(position > 30){
-        setSticky(true)
-      }else{setSticky(false)}
-    }
+    const handleScroll = () => {
+      const position = Math.floor(window.scrollY);
+
+      if (position > 30) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [ref.current]);
-  console.log(sticky)
+  console.log(sticky);
+  const parent = {
+    hidden: { opacity: 0, rotate: 180 },
+    visible: { opacity: 1, rotate: 0, transition: { duration: 2 } },
+  };
+  const anim = {
+    hidden: {
+      opacity: 0,
+      pathLength: 0,
+    },
+    visible: {
+      opacity: 1,
+      pathLength: 1,
+      transition: {
+        duration: 4,
+        ease: "easeInOut",
+      },
+    },
+  };
   return (
     <div className="relative container mx-auto flex items-start   justify-start gap-2 pt-0 box-border">
       {/* sidebar container*/}
       <div className="h-[calc(100vh-5.5rem)] thin-box-divider sticky top-20  pl-0 box-border flex justify-center items-start w-[26rem] overflow-y-auto pt-14 ">
         {/* content */}
         <nav className="relative h-full  px-[1rem]">
-          <h5 className="text-slate-900 text-sm  text-left font-inter  font-bold capitalize mb-4">
-            getting started 
+          <h5 className="text-slate-900 text-sm  text-left font-plus  font-bold capitalize mb-4">
+            getting started
+            {/* <svg
+              className="medium-box-divider "
+              viewBox="0 0 48 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <motion.path
+                initial={{ pathLength: 0 }}
+                animate={{ pathLength: 1 }}
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                }}
+                id="SVGRepo_iconCarrier"
+                d="M 5.99992 24 H 41.9999"
+                stroke="rgb(15, 23, 42)"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <motion.path
+                initial={{ pathLength: 0 }}
+                animate={{ pathLength: 1 }}
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  repeatDelay: 1,
+                }}
+                id="SVGRepo_iconCarrier_head"
+                d="M 30 12 L 42 24 L 30 36"
+                stroke="red"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg> */}
           </h5>
           {/* items */}
-          <div className="space-y-3.5 text-slate-700 pl-3 text-left  font-inter">
+          <div className="space-y-3.5 text-slate-700 pl-3 text-left  font-plus">
             <p className="capitalize text-sm font-semibold  cursor-pointer hover:text-blue-500 ">
               {textContent.title}
             </p>
@@ -54,7 +112,9 @@ const SidebarLayoutPage = () => {
           <p className="text-sm leading-[24px]  font-semibold mb-3 capitalize lg:mb-3 text-blue-500">
             {textContent.category}
           </p>
-          <h1 className={`inline-block text-2xl sm:text-3xl font-sans font-semibold text-slate-900  capitalize`}>
+          <h1
+            className={`inline-block text-2xl sm:text-3xl font-plus font-bold tracking-tight text-slate-900  capitalize`}
+          >
             {textContent.title}
           </h1>
           <p className="mt-4 lg:mt-6 text-lg ">{textContent.detail}</p>
