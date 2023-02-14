ce4e86d1d044aba5480ec641409cd6bee8997456
rogel cristobal <rogel.cristobal11@gmail.com>
2023-02-15 07:51:23 +0800

    optional threshold

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,48 +1,63 @@
 import React from "react";
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import DocsContext from "../context/DocsContext";
 const Section = ({ title, children, category, child, itemID, hash }) => {
-  const { ref, entry,inView} = useInView()
-  const {setState,state} = useContext(DocsContext)
+  const [threshold, setThreshold] = useState(null);
+  const { ref, entry, inView } = useInView({
+    threshold: threshold,
+  });
+  const { setState } = useContext(DocsContext);
   // text? 1 word=> return text: camel-case
-  const camelCaseAndRemoveSpaces=(value)=>{
-    if(value.split(" ").Length === 1){
-       console.log(value)
-    }else{
-      const converted = value.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+  const camelCaseAndRemoveSpaces = (value) => {
+    if (value.split(" ").Length === 1) {
+      console.log(value);
+    } else {
+      const converted = value
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
           return index == 0 ? word.toLowerCase() : word.toUpperCase();
         })
         .replace(/\s+/g, "");
 
-        return converted
+      return converted;
     }
-  }
+  };
   // [inView] state.viewState = inView value
-  useEffect(()=>{
-    setState(prev=> ({
+  useEffect(() => {
+    setState((prev) => ({
       ...prev,
-      sections: prev.sections.map((item,id)=>{
-        if(id === itemID){
-          return { ...item, viewState: inView}
+      sections: prev.sections.map((item, id) => {
+        if (id === itemID) {
+          return { ...item, viewState: inView };
         }
-        return item
-      })      
-    }))
-  },[inView])
+        return item;
+      }),
+    }));
+  }, [inView]);
 
-  
+  useEffect(() => {
+    if (entry) {
+      if (entry.target.clientHeight > 650) {
+        setThreshold(0.3);
+      } 
+      else {
+        setThreshold(0.7)
+      
+      }
+    }
+  }, [entry]);
   return (
     <div
       {...(category && { ref: ref })}
-      {...(entry&& { id: category})}   
+      {...(entry && { id: category })}
       className="flex-auto  box-border pb-4 pt-6 relative scroll-mt-20 "
     >
       {category && (
         <p
           className={` thin-box-divider w-fit  transition-all duration-300 ease-in-out   z-30 text-blue-500 text-base  font-bold   leading-[24px] tracking-tight  mb-3 capitalize lg:mb-3  font-plus`}
         >
-          {category}{itemID}
+          {category}
+          {threshold}
         </p>
       )}
 
--- src/components/SidebarLayoutPage.jsx
+++ src/components/SidebarLayoutPage.jsx
@@ -2,29 +2,27 @@ import React from "react";
 import { useContext, useRef, useState, useEffect } from "react";
 import DocsContext from "../context/DocsContext";
 import Section from "./Section";
-import {useNavigate} from 'react-router-dom'
 const SidebarLayoutPage = () => {
-  const { state: textContent } = useContext(DocsContext);
-  const ref = useRef(null);
-  const [sticky, setSticky] = useState(false);
+  const {state} = useContext(DocsContext);
+  // const ref = useRef(null);
+  // const [ setSticky] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = Math.floor(window.scrollY);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const position = Math.floor(window.scrollY);
 
-      if (position > 30) {
-        setSticky(true);
-      } else {
-        setSticky(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [ref.current]);
+  //     if (position > 30) {
+  //       setSticky(true);
+  //     } else {
+  //       setSticky(false);
+  //     }
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [ref.current]);
  
-  const [targetId, setTargetId] = useState(null);
   
   const targetRef = React.useRef(null);
  
@@ -54,38 +52,39 @@ const SidebarLayoutPage = () => {
           </p>
           {/* items */}
           <div className="space-y-3.5 text-slate-700 pl-3 text-left flex items-start flex-col justify-start  font-plus">
-            <a href="#introduction" className="capitalize text-sm font-semibold  cursor-pointer text-blue-500 ">
-              {textContent.title}
+            <a href="#introduction" className="capitalize text-sm font-semibold  cursor-pointer  ">
+              {state.title}
             </a>
-            {textContent.sections.map((item, id) => (
+            {state.sections.map((item, id) => (
               <a
                 key={id}
                 href={`#${item.category}`}
-                className="capitalize text-sm font-semibold  cursor-pointer hover:text-blue-500 text-slate-500/70"
+                className={`capitalize text-sm font-semibold  cursor-pointer ${!item.viewState? 'text-slate-900': ' text-blue-500'}`}
               >
-                {item.title}{String(item.viewState)}
+                {item.title}
               </a>
             ))}
           </div>
         </nav>
       </div>
+
       {/* body */}
       <div ref={targetRef} className="h-full box-border thin-box-divider w-full py-14 px-12  ">
         {/* heading container */}
         <div id="introduction" className="scroll-mt-28 max-w-3xl relative flex-auto  mb-14">
           <p className="text-sm leading-[24px]  font-semibold mb-3 capitalize lg:mb-3 text-blue-500">
-            {textContent.category}
+            {state.category}
           </p>
           <h1
             className={`inline-block text-2xl sm:text-3xl font-plus font-bold tracking-tight text-slate-900  capitalize`}
           >
-            {textContent.title}
+            {state.title}
           </h1>
-          <p className="mt-4 lg:mt-6 text-lg ">{textContent.detail}</p>
+          <p className="mt-4 lg:mt-6 text-lg ">{state.detail}</p>
         </div>
         {/* content */}
 
-        {textContent.sections.map((item, id) => (
+        {state.sections.map((item, id) => (
           <Section
             key={id}
             itemID={id}
